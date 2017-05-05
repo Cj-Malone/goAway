143cca431ace9ceb0b06ccf7efaf8f300a316e35
Cj Malone <CjMalone@mail.com>
2017-05-05 10:57:57 +0100

    Add install script

---
+++ goaway.sh
@@ -0,0 +1,13 @@
+#!/usr/bin/env bash
+
+CACHEDIR=${XDG_CACHE_HOME:=$HOME/.cache/}/goaway
+OUTDIR=${XDG_DATA_HOME:=$HOME/.local/share/}/applications
+
+if [ -e $CACHEDIR/ ]; then
+	cd $CACHEDIR
+	git pull
+else
+	git clone https://github.com/Cj-Malone/goAway.git $CACHEDIR
+fi
+
+cp $CACHEDIR/desktops/*.desktop $OUTDIR/
